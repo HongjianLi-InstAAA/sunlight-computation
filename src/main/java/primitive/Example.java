b027de8ca5b2ad2816bc5bbd0083f72ef8c90ef1
Jiaqian Wu <wuchiachien@163.com>
2021-01-26 16:36:07 +0800

    Initial commit

---
+++ src/main/java/primitive/Example.java
@@ -0,0 +1,113 @@
+package primitive;
+
+import gzf.gui.CameraController;
+import gzf.gui.Vec_Guo;
+import org.locationtech.jts.geom.Geometry;
+import processing.core.PApplet;
+
+@SuppressWarnings("serial")
+public class Example extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("primitive.Example");
+	}
+
+	CameraController cam;
+	SolarCalculator cal = new SolarCalculator(30);
+	JTSRender render = new JTSRender(this);
+
+	Vec_Guo[] shape = new Vec_Guo[] { new Vec_Guo(-5, -5), new Vec_Guo(5, -5), new Vec_Guo(5, 2), new Vec_Guo(0, 2), new Vec_Guo(0, 5), new Vec_Guo(-5, 5) };
+	Vec_Guo[] shape2 = new Vec_Guo[] { new Vec_Guo(5, 5), new Vec_Guo(15, 5), new Vec_Guo(15, 12), new Vec_Guo(10, 12), new Vec_Guo(10, 15), new Vec_Guo(5, 15) };
+
+	float shapeHeight = 10;
+
+	public void settings() {
+		size(800, 800, P3D);
+	}
+	public void setup() {
+		cam = new CameraController(this, 10);
+		cal.setDate(12, 22);
+	}
+
+	public void draw() {
+		background(255);
+		cam.drawSystem(10);
+		cal.setDate((double) mouseX / width);
+		/*
+		 * draw solar position
+		 */
+		Vec_Guo pos = cal.getSolarPosition((double) mouseY / height);
+		pos.mul(20);
+		if (pos.z > 0)
+			stroke(255, 0, 0);
+		else
+			stroke(0, 0, 0);
+		strokeWeight(15);
+		point((float) pos.x, (float) pos.y, (float) pos.z);
+
+		/*
+		 * draw shadow
+		 */
+		Geometry shadow = ShadowCalculator.getShadowOnGroundPrecise(pos, shape, shapeHeight, 0);
+		Geometry shadow2 = ShadowCalculator.getShadowOnGroundPrecise(pos, shape2, shapeHeight, 0);
+		if (shadow != null) {
+			fill(0x30000000);
+			noStroke();
+			render.draw(shadow);
+		}
+		
+		if (shadow2 != null) {
+			fill(0x30000000);
+			noStroke();
+			render.draw(shadow2);
+		}
+
+		/*
+		 * draw building
+		 */
+		fill(255);
+		stroke(0);
+		strokeWeight(1);
+		drawShape();
+		
+		drawShape2();
+	}
+
+	void drawShape() {
+		int len = shape.length;
+		for (int i = 0; i < len; i++) {
+			int n = (i + 1) % len;
+			beginShape();
+			vertex((float) shape[i].x, (float) shape[i].y);
+			vertex((float) shape[i].x, (float) shape[i].y, shapeHeight);
+			vertex((float) shape[n].x, (float) shape[n].y, shapeHeight);
+			vertex((float) shape[n].x, (float) shape[n].y);
+			endShape(CLOSE);
+		}
+
+		beginShape();
+		for (int i = 0; i < len; i++) {
+			vertex((float) shape[i].x, (float) shape[i].y, shapeHeight);
+		}
+		endShape(CLOSE);
+	}
+	
+	void drawShape2() {
+		int len = shape2.length;
+		for (int i = 0; i < len; i++) {
+			int n = (i + 1) % len;
+			beginShape();
+			vertex((float) shape2[i].x, (float) shape2[i].y);
+			vertex((float) shape2[i].x, (float) shape2[i].y, shapeHeight);
+			vertex((float) shape2[n].x, (float) shape2[n].y, shapeHeight);
+			vertex((float) shape2[n].x, (float) shape2[n].y);
+			endShape(CLOSE);
+		}
+
+		beginShape();
+		for (int i = 0; i < len; i++) {
+			vertex((float) shape2[i].x, (float) shape2[i].y, shapeHeight);
+		}
+		endShape(CLOSE);
+	}
+}
---
+++ src/main/java/primitive/ShadowCalculator.java
@@ -0,0 +1,147 @@
+package primitive;
+
+
+import gzf.gui.Vec_Guo;
+import org.locationtech.jts.geom.*;
+
+public class ShadowCalculator {
+	private static final GeometryFactory gf = new GeometryFactory();
+
+	private static Polygon toPolygon(Vec_Guo[] v) {
+		int len = v.length;
+		Coordinate[] coord = new Coordinate[len + 1];
+		for (int i = 0; i < len; i++) {
+			coord[i] = new Coordinate(v[i].x, v[i].y);
+		}
+		coord[len] = coord[0];
+
+		return gf.createPolygon(coord);
+	}
+
+	/**
+	 * counterclockwise shape required, less accurate but more efficient
+	 * 
+	 * @param light
+	 * @param shape
+	 * @param shapeHeight
+	 * @param groundHeight
+	 * @return
+	 */
+	public static Geometry getShadowOnGroundRough(Vec_Guo light, Vec_Guo[] shape, double shapeHeight, double groundHeight) {
+		if (shapeHeight < groundHeight)
+			return null;
+		if (shapeHeight < 0)
+			throw new IllegalArgumentException("shape height must be positive!");
+		if (light.z >= 0)
+			return null;
+
+		int len = shape.length;
+
+		double scale = (shapeHeight - groundHeight) / Math.abs(light.z);
+
+		Vec_Guo lightGround = light.dup().z(0).mul(scale);
+
+		Vec_Guo lightNormal = Vec_Guo.zaxis.cross(lightGround).unit();
+
+		
+		double maxP = Double.NEGATIVE_INFINITY;
+		double minP = Double.POSITIVE_INFINITY;
+
+		// right
+		int minIndex = -1;
+		// left
+		int maxIndex = -1;
+
+		for (int i = 0; i < len; i++) {
+			double p = shape[i].dot(lightNormal);
+			if (p > maxP) {
+				maxP = p;
+				maxIndex = i;
+			}
+
+			if (p < minP) {
+				minP = p;
+				minIndex = i;
+			}
+		}
+
+		Vec_Guo[] shapeOut = new Vec_Guo[len + 2];
+
+		int count = 0;
+		boolean add = false;
+		for (int i = 0; i < len; i++) {
+			// start from the right
+			int index = (i + minIndex) % len;
+			if (index == minIndex) {
+				shapeOut[count] = shape[index];
+				count++;
+				shapeOut[count] = shape[index].dup().add(lightGround);
+				add = true;
+			} else if (index == maxIndex) {
+				shapeOut[count] = shape[index].dup().add(lightGround);
+				count++;
+				shapeOut[count] = shape[index];
+				add = false;
+			} else {
+				if (add) {
+					shapeOut[count] = shape[index].dup().add(lightGround);
+				} else {
+					shapeOut[count] = shape[index];
+				}
+			}
+			count++;
+		}
+		
+		return toPolygon(shapeOut);
+	}
+
+	/**
+	 * accurate but slow
+	 * 
+	 * @param lig
+	 * @param shape
+	 * @param shapeHeight
+	 * @param groundHeight
+	 * @return
+	 */
+	public static Geometry getShadowOnGroundPrecise(Vec_Guo lig, Vec_Guo[] shape, double shapeHeight, double groundHeight) {
+		Vec_Guo light = new Vec_Guo(-lig.x,-lig.y,-lig.z);
+		if (shapeHeight < groundHeight)
+			return null;
+		if (shapeHeight < 0)
+			throw new IllegalArgumentException("shape height must be positive!");
+		if (light.z >= 0)
+			return null;
+
+		int len = shape.length;
+
+		double scale = (shapeHeight - groundHeight) / Math.abs(light.z);
+
+		Vec_Guo lightGround = light.dup().z(0).mul(scale);
+
+		Geometry[] geos = new Geometry[len + 1];
+		for (int i = 0; i < len; i++) {
+			int n = (i + 1) % len;
+			Vec_Guo[] shadow = new Vec_Guo[4];
+			shadow[0] = shape[i];
+			shadow[1] = shape[n];
+			shadow[2] = shape[n].dup().add(lightGround);
+			shadow[3] = shape[i].dup().add(lightGround);
+
+			geos[i] = toPolygon(shadow);
+		}
+
+		Vec_Guo[] shadow = new Vec_Guo[len];
+		for (int i = 0; i < len; i++) {
+			shadow[i] = shape[i].dup().add(lightGround);
+		}
+
+		geos[len] = toPolygon(shadow);
+
+		GeometryCollection gc = gf.createGeometryCollection(geos);
+		Geometry buffer = gc.buffer(0);
+
+		return buffer;
+	}
+
+}
